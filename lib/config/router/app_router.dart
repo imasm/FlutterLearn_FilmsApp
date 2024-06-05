517270823f241447b759965416979f781902f092
Ivan Masmitjà <imasmitja@gmail.com>
2024-06-05 11:38:48 +0200

    Change to StatefulShellRoute: movie details doesn't work

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,29 +1,50 @@
-import 'package:flutter/material.dart';
 import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:cinemapedia/presentation/views/views.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
-  ShellRoute(
-      builder: (context, state, child) {
-        return HomeScreen(childView: child);
+final GlobalKey<NavigatorState> _rootNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'root');
+
+final GlobalKey<NavigatorState> _tabANavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'tabANav');
+
+final appRouter = GoRouter(
+  navigatorKey: _rootNavigatorKey,
+  initialLocation: '/', 
+  routes: [
+  StatefulShellRoute.indexedStack(
+      builder: (BuildContext context, GoRouterState state, StatefulNavigationShell navigationShell) {
+        return HomeScreen(navigationShell: navigationShell);
       },
-      routes: [
-        GoRoute(path: "/", builder: (context, state) => const HomeView(), 
-        routes: [
-          GoRoute(
-            path: "movie/:id",
-            name: MovieScreen.name,
-            builder: (context, state) {
-              final movieId = state.pathParameters['id'] ?? "no-id";
-              return MovieScreen(movieId: movieId);
-            },
-          ),
-        ]),
-        
-        GoRoute(path: "/favorites", builder: (context, state) => const FavoritesView()),
-        GoRoute(path: "/categories",  builder: (context, state) => const CategoriesView()),
+      branches: <StatefulShellBranch>[
+        StatefulShellBranch(navigatorKey: _tabANavigatorKey, routes: [homeViewRoute()]),
+        StatefulShellBranch(routes: [favoritesViewRoute()]),
+        StatefulShellBranch(routes: [categoriesViewRoute()])
+
+        //GoRoute(path: "/favorites", builder: (context, state) => const FavoritesView()),
+        //GoRoute(path: "/categories",  builder: (context, state) => const CategoriesView()),
       ])
- 
 ]);
 
+
+GoRoute homeViewRoute() {
+  return GoRoute(path: "/", builder: (context, state) => const HomeView(), routes: [
+            GoRoute(
+              path: "movie/:id",
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? "no-id";
+                return MovieScreen(movieId: movieId);
+              },
+            ),
+          ]);
+}
+
+GoRoute favoritesViewRoute() {
+  return GoRoute(path: "/categories", builder: (context, state) => const CategoriesView());
+}
+
+GoRoute categoriesViewRoute() {
+  return GoRoute(path: "/favorites", builder: (context, state) => const FavoritesView());
+}
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
+import 'package:go_router/go_router.dart';
 
 
 // This is the home screen of the app.
 // It shows a list of movies in a slideshow and several horizontal lists of movies.
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key, required this.childView});
+  const HomeScreen({super.key, required this.navigationShell});
 
   static const name = 'home-screen';
-  final Widget childView;
+  final StatefulNavigationShell navigationShell;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  childView,
+      body:  navigationShell,
       bottomNavigationBar: const CustomBottomNavigation(),
     );
   }
