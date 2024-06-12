22754df6ac19ad3eebb9f5638f5adc3a76e81382
Ivan Masmitjà <imasmitja@gmail.com>
2024-06-12 11:24:00 +0200

    Extract gradient to custom widget

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,5 +1,6 @@
-export 'shared/custom_avigation_bar.dart';
-export 'shared/custom_appbar.dart';
 export 'movies/movies_slideshow.dart';
 export 'movies/movies_horizontal_listview.dart';
+export 'shared/gradient_layer.dart';
+export 'shared/custom_avigation_bar.dart';
+export 'shared/custom_appbar.dart';
 export 'shared/full_screen_loader.dart';
