5955102ed720b9b0c31b803121231611537dc09f
Ivan Masmitjà <imasmitja@gmail.com>
2025-04-25 09:25:30 +0200

    Isar or SQLite configurable a Environment

--- lib/presentation/providers/favorites/favorites_repository_provider.dart
+++ lib/presentation/providers/favorites/favorites_repository_provider.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/domain.dart';
 import 'package:cinemapedia/infrastructure/localdb/datasources/isar_favorites_datasource.dart';
 import 'package:cinemapedia/infrastructure/localdb/datasources/sqlite_favorites_datasource.dart';
@@ -5,6 +6,10 @@ import 'package:cinemapedia/infrastructure/repositories/favorites_repository_imp
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final favoritesRepositoryProvider = StateProvider<FavoritesRepository>((ref) {
+  if (Environment.useIsar) {
+    return FavoritesRepositoryImpl(favoritesDatasource: IsarFavoritesDatasource());
+  }
+  // Fallback to SQLite if Isar is not used
   return FavoritesRepositoryImpl(favoritesDatasource: SqliteFavoritesDatasource());
 });
 
